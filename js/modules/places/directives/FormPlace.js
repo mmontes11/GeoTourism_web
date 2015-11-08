7fc2ed74a09596b85f048056214c003424f9232a
Martin <martin11lrx@gmail.com>
2015-11-08 19:36:50 +0100

    Form place dialog directive improved. Filters selection look n' feel improved.

--- js/modules/places/directives/FormPlace.js
+++ js/modules/places/directives/FormPlace.js
@@ -3,16 +3,43 @@
 define([
     '../module'
 ], function (module) {
-   module.directive('formPlace', function(){
-       return {
-           restrict: 'E',
-           replace: true,
-           templateUrl: 'partials/places/formPlace.html',
-           scope: {
-               form: "=",
-               place: "=",
-               types: "="
-           }
-       };
-   });
+    module.directive('formPlace', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'partials/places/formPlace.html',
+            scope: {
+                form: "=",
+                edit: "=",
+                place: "=",
+                restoreplace: "=",
+                types: "="
+            },
+            link: function (scope, element, attrs) {
+                scope.place = scope.place || {};
+                scope.edit = eval(scope.edit);
+                console.log(scope.edit);
+
+                scope.$watch('selectedsearchitem', function (item) {
+                    if (angular.isDefined(item)) {
+                        scope.place.infoUrl = item.url;
+                    } else {
+                        if (angular.isDefined(scope.restoreplace)){
+                            scope.place.infoUrl = angular.copy(scope.restoreplace.infoUrl);
+                        }else{
+                            scope.place.infoUrl = undefined;
+                        }
+
+                    }
+                });
+
+                scope.$watch('uploadedurl', function (uploadedUrl) {
+                    if (angular.isDefined(uploadedUrl)) {
+                        scope.place.photoUrl = uploadedUrl;
+                        scope.form.$dirty = true;
+                    }
+                });
+            }
+        };
+    });
 });
