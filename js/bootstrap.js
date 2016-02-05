33f5a1d2545a1090f58a81901b01077b1085fdab
Martin <martin11lrx@gmail.com>
2016-02-05 21:25:55 +0100

    Google Analytics added.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -7,7 +7,8 @@ define([
     './config/constants',
     './config/interceptors',
     './config/routes',
-    './config/libraries'
+    './config/libraries',
+    './config/analytics'
 ], function(require,angular){
     require(['domReady!'], function(document) {
         angular.bootstrap(document, ['app']);
